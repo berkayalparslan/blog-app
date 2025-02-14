17ed1fbfb03649020ff651418e0a7e7c21bdc2ea
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2025-02-14 16:58:18 +0100

    added posts endpoints

--- routes/posts.js
+++ routes/posts.js
@@ -19,4 +19,33 @@ router.get('/', function(req, res, next) {
   return res.json(posts);
 });
 
+router.get('/:id', function(req, res, next) {
+  const post = posts.find(post => post.id === parseInt(req.params.id));
+  if (!post) {
+    return res.status(404).json({ message: 'Post not found' });
+  }
+  return res.json(post);
+});
+
+router.post('/', function(req, res, next) {
+  const post = {
+    id: posts.length + 1,
+    title: req.body.title,
+    content: req.body.content,
+    createdDate: new Date()
+  };
+  posts.push(post);
+  return res.status(201).json(post);
+});
+
+router.put('/:id', function(req, res, next) {
+  const post = posts.find(post => post.id === parseInt(req.params.id));
+  if (!post) {
+    return res.status(404).json({ message: 'Post not found' });
+  }
+  post.title = req.body.title;
+  post.content = req.body.content;
+  return res.json(post);
+});
+
 module.exports = router;
